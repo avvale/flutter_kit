495511f0ff0195a71482b8e5999ae9acbe157417
Alejandro <alexelninja@gmail.com>
2023-10-04 15:39:01 +0200

    Testing something

--- lib/src/utils/examples/main.dart
+++ lib/src/utils/examples/main.dart
@@ -1,6 +1,6 @@
+import 'package:flutter_kit/flutter_kit.dart';
 import 'package:flutter_kit/src/utils/examples/api_repository.dart';
 import 'package:flutter_kit/src/utils/helpers.dart';
-import 'package:flutter_kit/src/widgets/common/fx_app.dart';
 import 'package:flutter_kit/src/widgets/common/space.dart';
 import 'package:graphql/client.dart';
 
--- lib/src/utils/toast.dart
+++ lib/src/utils/toast.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_kit/src/widgets/common/fx_app.dart';
+import 'package:flutter_kit/flutter_kit.dart';
 
 enum ToastMode {
   success,
