022d5d4550049df248416ccbde9f121c7264dd5b
Alejandro <alexelninja@gmail.com>
2023-10-16 12:22:29 +0200

    renaming

--- lib/widgets/space.dart
+++ lib/widgets/space.dart
@@ -6,19 +6,19 @@ class Space extends StatelessWidget {
   /// Space size. This value is multiplied by 4 to get the size in pixels.
   final double size;
 
-  /// Space direction.
-  final SpaceAxis direction;
+  /// Axis to apply space.
+  final SpaceAxis axis;
 
   /// Space between widgets.
   const Space({
     Key? key,
     this.size = 0,
-    this.direction = SpaceAxis.vertical,
+    this.axis = SpaceAxis.vertical,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    switch (direction) {
+    switch (axis) {
       case SpaceAxis.vertical:
         return SizedBox(height: size * 4);
       case SpaceAxis.horizontal:
